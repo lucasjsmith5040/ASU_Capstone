47845bc2db0e4705311efbf712553d2b10d95840
Daniel Bihnam <daniel.bihnam@gmail.com>
2025-04-09 18:50:13 -0700

    Site-specific SFS analysis

---
+++ geneticData/scripts/mergeSamples_location.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Check if the sample file argument is provided
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <sample_file.txt>"
+    exit 1
+fi
+
+#Use user-specified location file
+sample_file=$1
+
+#Initialize the list of VCFs to be merged
+vcf_list=""
+
+#Loop through the sample IDs in the provided sample file
+while read sample; do
+    vcf_list="$vcf_list ${sample}.filtered.vcf.gz"
+done < "$sample_file"
+
+#Merge vcfs
+bcftools merge $vcf_list -o merged237/location/merged_${sample_file%.txt}.vcf.gz -Oz
---
+++ geneticData/scripts/sfsLocation.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#Load vcftools
+module load vcftools-0.1.14-gcc-11.2.0
+
+#Directory containing VCF files
+vcf_dir="/scratch/dbihnam/lsc585/turtleProject/variants/filtered_AF/combined/merged237/location"
+
+#Loop through each VCF file in the directory (assuming they are .vcf.gz files)
+for vcf_file in $vcf_dir/*.vcf.gz; do
+    base_filename=$(basename "$vcf_file" .vcf.gz)
+    #Calculate SFS for each VCF file
+    vcftools --gzvcf "$vcf_file" --freq --out "$vcf_dir/sfs_results_$base_filename"
+    # Split the last two columns for R, for each VCF
+    awk 'BEGIN {OFS="\t"} {split($5, a, ":"); print $1, $2, $3, $4, a[1], a[2]}' "$vcf_dir/sfs_results_$base_filename.frq" > "$vcf_dir/../sfs/location/sfs_preprocessed_$base_filename.frq"
+done
