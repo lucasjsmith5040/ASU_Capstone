c3f9ad4d7aa29092fbe97643f36d493e2cc3aee2
Daniel Bihnam <daniel.bihnam@gmail.com>
2025-04-11 19:04:23 -0700

    Add mapping data

---
+++ geneticData/scripts/locationMutCounts.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+#Navigate to directory
+cd /scratch/dbihnam/lsc585/turtleProject/variants/filtered_AF/combined/merged237/location
+
+#Match all vcfs
+vcf_files=(merged_*_samples.vcf.gz)
+
+#Set output file
+output_file="sampleLocationMutCounts.tsv"
+
+#Write output header
+echo -e "Region\tAverage_Variants_Per_Sample\tNum_Samples" > "$output_file"
+
+#Loop through all vcfs
+for vcf in "${vcf_files[@]}"; do
+  region=$(basename "$vcf" | sed 's/\.[^.]*$//')
+  num_samples=$(bcftools query -l "$vcf" | wc -l)
+  avg=$(bcftools query -f '[%GT\t]\n' "$vcf" | \
+  awk '
+  {
+    for(i=1; i<=NF; i++) {
+      if($i != "./." && $i != "." && $i != ".|.") count[i]++
+    }
+  }
+  END {
+    total=0;
+    for(i in count) {
+      total += count[i];
+    }
+    if(length(count) > 0)
+      print total/length(count);
+    else
+      print 0;
+  }')
+
+#Append results to output
+  echo -e "$region\t$avg\t$num_samples" >> "$output_file"
+done
