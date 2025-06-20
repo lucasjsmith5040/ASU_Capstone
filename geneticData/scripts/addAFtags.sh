a262d7d6de07a8be6e7aac3f7a1187aa24bb808d
dbihnam17 <132006653+dbihnam17@users.noreply.github.com>
2025-06-20 07:55:13 -0700

    Update addAFtags.sh
    Improve readability

--- geneticData/scripts/addAFtags.sh
+++ geneticData/scripts/addAFtags.sh
@@ -10,26 +10,26 @@
 #SBATCH --mail-user=dbihnam@asu.edu
 #SBATCH --mail-type=ALL
 
-#This script is intended to add allele frequency (AF) annotation to the INFO
-#column of the vcf files
+## This script is intended to add allele frequency (AF) annotation to the INFO
+## column of the vcf files
 
-#Load bcftools
+# Load bcftools
 module load bcftools-1.14-gcc-11.2.0
 
-#Set file paths
+# Set file paths
 VCF_DIR="/scratch/dbihnam/lsc585/turtleProject/variants/unfiltered"
 OUTPUT_DIR="/scratch/dbihnam/lsc585/turtleProject/variants/unfiltered_AF"
 SAMPLES_TXT="/scratch/dbihnam/lsc585/turtleProject/rawData/Trimmed/paired/samples.txt"
 
-#Create out directory if it doesn't exist
+# Create out directory if it doesn't exist
 mkdir -p "$OUTPUT_DIR"
 
-#Get the sample name using SLURM array task ID
+# Get the sample name using SLURM array task ID
 SAMPLE=$(sed -n "${SLURM_ARRAY_TASK_ID}p" "$SAMPLES_TXT")
 
-#File paths
+# File paths
 INPUT_VCF="${VCF_DIR}/${SAMPLE}.raw.vcf"
 OUTPUT_VCF="${OUTPUT_DIR}/${SAMPLE}.raw_with_AF.vcf"
 
-#Use fill-tags to add AF to the INFO field
+# Use fill-tags to add AF to the INFO field
 bcftools +fill-tags "$INPUT_VCF" -o "$OUTPUT_VCF" -- -t AF
